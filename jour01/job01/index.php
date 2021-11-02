996cfbd734160cdbfe634b7669231fa4c9f2f080
Pierre <pierre.galbes@laplateforme.io>
2021-11-02 17:08:02 +0100

    jour1

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+    $str = "LaPlateforme";
+    $str2 = "vive";
+    $str3 = "!";
+    echo"$str2 $str $str3";
+?>
+<br>
+<?php
+    $val = 6;
+    $val = $val +4;
+    echo $val;
+?>
+<br>
+<?php
+    $myBool = true;
+    echo "$myBool";
+
+    $myBool = false;
+    echo "$myBool"
+?>   
+    
+
---
+++ jour01/job02/index.php
@@ -0,0 +1,10 @@
+<?php
+$cookie = "Hello" ;
+$cookie2 = " " ;
+$cookie3 = "LaPlateforme!" ;
+echo "$cookie $cookie2 $cookie3 <br> ";
+//Commentaire 1 //
+
+echo 'Hello' , " " , 'LaPlateforme!';
+//Commentaire 2 //
+?>
